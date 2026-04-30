1dacc8d240e1761f974ac6ef7e5306eb6bd7a7d2
iqbal_dzaky <iqbal_muhammad_dzaky@teknokrat.ac.id>
2026-04-30 19:36:06 +0700

    commit saya

--- my-app/app/pelayanan/page.jsx
+++ my-app/app/pelayanan/page.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function pelayanan() {
   return (
-    <div>Ini Halaman PelayananIni Halaman PelayananIni Halaman Pelayanan</div>
+    <div>Ini Halaman PelayananIni Halaman PelayananIni Halaman Pelayanan pelayanan yang dilakukan sangat baik</div>
   )
 }
