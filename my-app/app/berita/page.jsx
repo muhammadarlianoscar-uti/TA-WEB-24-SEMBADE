443ed55579b05ea55316d7f47f2d3b6866461e34
iqbal_dzaky <iqbal_muhammad_dzaky@teknokrat.ac.id>
2026-05-04 19:44:23 +0700

    update halaman berita

--- my-app/app/berita/page.jsx
+++ my-app/app/berita/page.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 
 export default function berita() {
   return (
-    <div>Ini Halaman Berita ini halaman berita viral</div>
-  )
+    <main className="bg-gray-100 min-h-screen px-6 py-10">
+      
+      {/* HEADER */}
+      <div className="max-w-7xl mx-auto mb-8">
+        <h1 className="text-4xl font-bold text-green-600">
+          Berita Desa
+        </h1>
+        <p className="text-gray-600 mt-2">
+          Menyajikan informasi terbaru tentang peristiwa, berita terkini,
+          dan artikel-artikel jurnalistik dari Desa Kersik
+        </p>
+      </div>
+    </main>
+  ); 
 }
