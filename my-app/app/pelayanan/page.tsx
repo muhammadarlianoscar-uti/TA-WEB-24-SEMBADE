42d73199520521b99483be255f9d029c70510b29
MKumam-tech <muhammad_khoiru_umam@teknokrat.ac.id>
2026-05-09 12:52:37 +0700

    umam : membuat form pengaduan

--- my-app/app/pelayanan/page.tsx
+++ my-app/app/pelayanan/page.tsx
@@ -1,7 +1,128 @@
-import React from 'react'
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
-export default function pelayanan() {
+export default function PelayananPage() {
+  const [open, setOpen] = useState(false);
+
+  const [form, setForm] = useState({
+    name: "",
+    category: "",
+    customCategory: "",
+    address: "",
+    description: "",
+    image: null as File | null,
+  });
+
+  const handleChange = (e: any) => {
+    const { name, value, files } = e.target;
+
+    if (name === "image") {
+      setForm({ ...form, image: files?.[0] || null });
+    } else {
+      setForm({ ...form, [name]: value });
+    }
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    const finalCategory =
+      form.category === "lainnya" ? form.customCategory : form.category;
+
+    const data = {
+      ...form,
+      category: finalCategory,
+    };
+
+    console.log("DATA:", data);
+    alert("Pengaduan berhasil dikirim");
+  };
+
   return (
-    <div>saya sedang mengerjakan pelayanan</div>
-  )
+    <div className="p-6 max-w-5xl mx-auto">
+      <h1 className="text-2xl font-bold mb-6 text-center">Pelayanan Desa</h1>
+
+
+      {/* ================= PENGADUAN ================= */}
+
+      <div>
+        <h2>Form Pengaduan Masyarakat</h2>
+
+        <form onSubmit={handleSubmit}>
+          {/* nama */}
+          <div className="grid md:grid-cols-2 gap-2">
+            <input
+              name="name"
+              placeholder="Nama"
+              onChange={handleChange}
+              className="w-full border p-2 rounded-lg bg-white"
+            />
+            {/* jenis pengaduan */}
+              <input
+              name="Jenis pengaduan"
+              placeholder="Jenis pengaduan"
+              onChange={handleChange}
+              className="w-full border p-2 rounded-lg bg-white"
+            />
+
+          </div>
+
+          {/* lainnya */}
+          {form.category === "lainnya" && (
+            <input
+              name="customCategory"
+              placeholder="Tulis jenis pengaduan"
+              onChange={handleChange}
+              className="w-full border p-2 rounded-lg bg-white mt-4"
+              required
+            />
+          )}
+
+          {/* alamat */}
+          <input
+            name="address"
+            placeholder="Alamat / Patokan Lokasi"
+            onChange={handleChange}
+            className="w-full border p-3 rounded-lg bg-white mt-4"
+            required
+          />
+
+          {/* deskripsi BESAR */}
+          <textarea
+            name="description"
+            placeholder="Deskripsikan pengaduan secara lengkap..."
+            onChange={handleChange}
+            className="w-full border p-4 rounded-lg mt-4 h-44 bg-white"
+            required
+          />
+
+          {/* upload */}
+          <div className="mt-4">
+            <label>
+              Upload Foto
+            </label>
+
+            <div>
+              <input
+                type="file"
+                name="image"
+                onChange={handleChange}
+                className="w-full text-sm"
+              />
+            </div>
+
+            <p className="text-xs mt-1">Maksimal 5MB (jpg/png)</p>
+          </div>
+
+          <button>
+            Kirim Pengaduan
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
+
+
+
