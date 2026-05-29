6649cc311d1c84edae06d5dd3c42f390a8986593
iqbal_dzaky <iqbal_muhammad_dzaky@teknokrat.ac.id>
2026-05-29 11:11:49 +0700

    menambahkan grid layout card potensi

--- my-app/app/potensi/page.tsx
+++ my-app/app/potensi/page.tsx
@@ -71,7 +71,8 @@ const filteredData = activeFilter === 'Semua' ? potensiData : potensiData.filter
         ))}
 
       </div>
-
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> 
+        </div>
     </section>
 
   </div>
