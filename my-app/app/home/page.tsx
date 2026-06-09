00c012a882083f5bd372f7aa6224e2c4b0e63e60
muhammadarlianoscar-uti <muhammmad_arlian_oscar@teknokrat.ac.id>
2026-06-09 15:41:06 +0700

    menambahkan tombol detail

--- my-app/app/home/page.tsx
+++ my-app/app/home/page.tsx
@@ -298,7 +298,8 @@ export default function Home() {
                 <span>📍 Poskesdes Terpadu RW 04</span>
               </p>
             </div>
-           </div>
+            <div className="text-sm font-semibold border border-slate-200 hover:border-emerald-500 hover:text-emerald-600 py-2 px-5 rounded-xl transition-colors w-full sm:w-auto text-center cursor-pointer">Detail</div>
+          </div>
         </div>
       </section>
 
