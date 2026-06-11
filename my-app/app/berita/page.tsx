49eccce7f61c5803eaac8374ece6864d69845212
iqbal_dzaky <iqbal_muhammad_dzaky@teknokrat.ac.id>
2026-06-12 04:50:43 +0700

    Menghapus jumlah view

--- my-app/app/berita/page.tsx
+++ my-app/app/berita/page.tsx
@@ -225,9 +225,7 @@ export default function Berita() {
                   <svg className="w-4 h-4 text-emerald-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                   </svg>
-                    {/* jumlah view */}
-                    <span>{item.views}</span>
-                  </div>
+                </div>
                 </div>
               </div>
             </div>
