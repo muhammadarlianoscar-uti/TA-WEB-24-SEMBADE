132d8c3f7f5a53434ece7aad459ead2cd18ca760
iqbal_dzaky <iqbal_muhammad_dzaky@teknokrat.ac.id>
2026-06-12 04:48:12 +0700

    Menambahkan ikon penulis pada informasi

--- my-app/app/berita/page.tsx
+++ my-app/app/berita/page.tsx
@@ -223,7 +223,7 @@ export default function Berita() {
                   {/* menampilkan informasi tembahan */}
                 <div className="mt-auto pt-4 border-t border-slate-100 flex justify-between items-center text-sm text-slate-500">
                   <svg className="w-4 h-4 text-emerald-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
                   </svg>
                     {/* jumlah view */}
                     <span>{item.views}</span>
